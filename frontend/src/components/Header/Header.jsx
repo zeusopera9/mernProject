fd80db67528f447a07dbe45792af89744197dc0d
zeusopera9 <zaidalimerchant@yahoo.com>
2023-09-29 03:12:59 +0530

    Part 1 22:06

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import {Container, Row, Button} from 'reactstrap'
+import { NavLink, Link } from 'react-router-dom'
+import logo from '../../assets/images/logo.png'
+import './header.css'
+
+const nav__links = [
+  {
+    path:'/home',
+    display:'Home'
+  },
+  {
+    path:'/about',
+    display:'About'
+  },
+  {
+    path:'/tours',
+    display:'Tours'
+  },
+]
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Container>
+        <Row>
+          <div className="nav__wrapper d-flex algin-items-center 
+          justify-content-between">
+            <div className="logo">
+              <img src={logo} alt="Logo" />
+            </div>
+
+            <div className="navigation">
+              <ul className="menu d-flex align-items-center gap-5">
+                {
+                  nav__links.map((item, index) => (
+                    <li className="nav__item" key={index}>
+                      <NavLink to={item.path} className={navClass=>navClass.
+                        isActive ? "active__link" : ""}>{item.display}</NavLink>
+                    </li>
+                  ))
+                }
+              </ul>
+            </div>
+            <div className="nav__right d-flex align-items-center gap-4">
+              <div className="nav__btns d-flex align-items-center gap-4">
+                <Button className='btn secondary_btn'>
+                  <Link to='/login'>
+                    Login
+                  </Link>
+                </Button>
+                <Button className='btn primary_btn'>
+                  <Link to='/register'>
+                    Regiser
+                  </Link>
+                </Button>
+              </div>
+              <span className="mobile__menu">
+                <i className="ri-menu-line"></i>
+              </span>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </header>
+  )
+}
+
+export default Header
