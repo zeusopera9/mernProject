287aafc78448a634b0405129f270d58e409cbc8c
Rylan Lewis <rylanlewis045@gmail.com>
2023-09-30 01:39:26 +0530

    45:51

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import '../styles/home.css'
-import {Container, Row, Col} from 'reactstrap'
-import heroImg from '../assets/images/hero-img01.jpg'
-import heroImg02 from '../assets/images/hero-img02.jpg'
-import heroVideo from '../assets/images/hero-video.mp4'
-import worldImg from '../assets/images/world.png'
-import Subtitle from '../shared/Subtitle'
-
+import React from 'react';
+import '../styles/home.css';
+import {Container, Row, Col} from 'reactstrap';
+import heroImg from '../assets/images/hero-img01.jpg';
+import heroImg02 from '../assets/images/hero-img02.jpg';
+import heroVideo from '../assets/images/hero-video.mp4';
+import worldImg from '../assets/images/world.png';
+import Subtitle from '../shared/Subtitle';
+import SearchBar from '../shared/SearchBar';
+import ServiceList from '../services/ServiceList';
 const Home = () => {
   return (
     <>
@@ -27,6 +28,34 @@ const Home = () => {
                   debitis, quibusdam cumque asperiores? Commodi!</p>
               </div>
             </Col>
+            <Col lg='2'>
+              <div className="hero_img-box">
+                <img src={heroImg}  alt="" />
+              </div>
+            </Col>
+            <Col lg='2'>
+              <div className="hero_img-box mt-4">
+                <video src={heroVideo}  alt="" controls/>
+              </div>
+            </Col>
+            <Col lg='2'>
+              <div className="hero_img-box mt-5">
+                <img src={heroImg02}  alt="" />
+              </div>
+            </Col>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+      {/*hero section start*/}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3">
+              <h5 className="services_subtitle">What we serve</h5>
+              <h2 className="services_title">We offer our best services</h2>
+            </Col>
+            <ServiceList />
           </Row>
         </Container>
       </section>
---
+++ frontend/src/services/ServiceList.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ServiceCard from './ServiceCard';
+import { Col } from 'reactstrap';
+
+import weatherImg from '../assets/images/weather.png';
+import guideImg from '../assets/images/guide.png';
+import customizationImg from '../assets/images/customization.png';
+
+
+const servicesData = [
+    {
+        imgUrl:weatherImg,
+        title:"Calculate Weather",
+        desc:"Lorem ipsum dolor",
+    },
+    {
+        imgUrl:guideImg,
+        title:"Best Tour Guide",
+        desc:"Lorem ipsum dolor",
+    },
+    {
+        imgUrl:customizationImg,
+        title:"Customization",
+        desc:"Lorem ipsum dolor",
+    },
+]
+
+
+const ServiceList =()=>{
+    return(
+        <>
+        {servicesData.map((item,index)=>(
+            <Col lg="3" key={index}>
+                <ServiceCard item={item} />
+            </Col>
+        ))}
+        </>
+    );
+};
+
+export default ServiceList;
