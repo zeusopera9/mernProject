7456b0bebdfc1936b83e34e03d94a80f945113aa
zeusopera9 <zaidalimerchant@yahoo.com>
2023-10-03 11:55:51 +0530

    Part 3 5:22

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,29 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Database Connection
+const connect = async()=> {
+    try {
+
+    } catch (err) {
+
+    }
+}
+
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser);
+
+
+app.listen(port, () => {
+    console.log('Server is listening on port', port);
+})
