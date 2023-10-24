29d3f6b14e27aaac5b8b500526de6adcbed7e2fe
zeusopera9 <zaidalimerchant@yahoo.com>
2023-10-24 11:50:30 +0530

    Part 3 24:05

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -15,4 +15,53 @@ export const createTour = async(req, res) => {
     } catch(err) {
         res.status(500).json({success:false, message:'Failed to Create. Try again'})
     }
-}
+};
+
+// Update tour
+export const updateTour = async(req, res) => {
+    const id = req.params.id
+
+    try {
+
+        const updatedTour = await Tour.findByIdAndUpdate(id,{
+            $set: req.body
+        }, {new:true})
+
+        res.status(200).json({success:true, message:'Successfully Updated', 
+        data:updateTour})
+
+    } catch(err) {
+        res.status(500).json({success:false, message:'Failed to Update'})
+    }
+    
+};
+
+// Delete tour
+export const deleteTour = async(req, res) => {
+    const id = req.params.id
+
+    try {
+
+        await Tour.findByIdAndDelete(id)
+
+        res.status(200).json({success:true, message:'Successfully Deleted'})
+
+    } catch(err) {
+        res.status(500).json({success:false, message:'Failed to Delete'})
+    }
+    
+};
+
+// getSingle tour
+export const getSingleTour = async(req, res) => {
+    try {
+
+    } catch(err) {}
+};
+
+// getAll tour
+export const getAllTour = async(req, res) => {
+    try {
+
+    } catch(err) {}
+};
--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -1,9 +1,21 @@
 import express from 'express'
-import { createTour } from '../controllers/tourController.js'
+import { createTour, deleteTour, getAllTour, getSingleTour, updateTour } from '../controllers/tourController.js'
 
 const router = express.Router()
 
 // Create new tour
 router.post('/', createTour)
+
+// Update tour
+router.put('/:id', updateTour)
+
+// Delete tour
+router.delete('/:id', deleteTour)
+
+// Get Single tour
+router.get('/:id', getSingleTour)
+
+// Get All tour
+router.get('/:id', getAllTour)
 
 export default router;
