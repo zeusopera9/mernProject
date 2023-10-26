e84f97fb1abba327fccd8a260d5a3fcf9da1616b
zeusopera9 <zaidalimerchant@yahoo.com>
2023-10-27 02:29:56 +0530

    Part 4 15:31

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -6,7 +6,7 @@ import './tour-card.css'
 
 const TourCard = ({tour}) => {
 
-    const {id, title, city, photo, price, featured , reviews} = tour;
+    const {_id, title, city, photo, price, featured , reviews} = tour;
     const{totalRating,avgRating}=calculateAvgRating(reviews);
   return (
     <div className='tour__card'>
@@ -29,7 +29,7 @@ const TourCard = ({tour}) => {
                 </div>
 
                 <h5 className="tour__title">
-                    <Link to={`/tours/${id}`}>{title}</Link>
+                    <Link to={`/tours/${_id}`}>{title}</Link>
                 </h5>
 
                 <div className="card__bottom d-flex align-items-center 
@@ -37,7 +37,7 @@ const TourCard = ({tour}) => {
                     <h5>${price}<span> /per person/</span></h5>
 
                     <button className="btn booking__btn">
-                        <Link to={`/tours/${id}`}>Book Now</Link>
+                        <Link to={`/tours/${_id}`}>Book Now</Link>
                     </button>
                 </div>
             </CardBody>
