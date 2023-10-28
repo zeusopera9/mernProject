36a58075fa2f79901a566de84d5201d22fff42b9
zeusopera9 <zaidalimerchant@yahoo.com>
2023-10-28 21:21:34 +0530

    HERE WE GO

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -25,6 +25,7 @@ const nav__links = [
 const Header = () => {
 
   const headerRef = useRef(null)
+  const menuRef = useRef(null)
   const navigate = useNavigate()
   const {user, dispatch} = useContext(AuthContext)
 
@@ -49,6 +50,8 @@ const Header = () => {
     return window.removeEventListener('scroll', stickyHeaderFunc)
   })
 
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
+
   return (
     <header className="header" ref={headerRef}>
       <Container>
@@ -59,7 +62,7 @@ const Header = () => {
               <img src={logo} alt="Logo" />
             </div>
 
-            <div className="navigation">
+            <div className="navigation" ref={menuRef} onClick={toggleMenu}>
               <ul className="menu d-flex align-items-center gap-5">
                 {
                   nav__links.map((item, index) => (
@@ -93,7 +96,7 @@ const Header = () => {
           
                 
               </div>
-              <span className="mobile__menu">
+              <span className="mobile__menu" onClick={toggleMenu}>
                 <i className="ri-menu-line"></i>
               </span>
             </div>
