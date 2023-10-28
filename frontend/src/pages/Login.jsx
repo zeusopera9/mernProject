e9ebf0c2b76e97da7e6bb7142a309978c9ea8620
zeusopera9 <zaidalimerchant@yahoo.com>
2023-10-28 15:50:11 +0530

    Part 4 35:00

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import {Container, Row, Col, Form, FormGroup, Button} from 'reactstrap'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import '../styles/login.css'
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 
 import loginImg from '../assets/images/login.png'
 import userIcon from '../assets/images/user.png'
+
+import {AuthContext} from './../context/AuthContext'
+import {BASE_URL} from './../utils/config'
 
 const Login = () => {
 
